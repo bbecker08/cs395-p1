5d10d8f6c2f48506352584bd71f01f2738d08d4b
Brian <bbecker@haverford.edu>
2018-09-29 18:34:48 -0400

    charm

--- app/src/test/java/edu/haverford/cs/squirrelfacts/SquirrelDoublyLinkedListTest.java
+++ app/src/test/java/edu/haverford/cs/squirrelfacts/SquirrelDoublyLinkedListTest.java
@@ -2,12 +2,18 @@ package edu.haverford.cs.squirrelfacts;
 
 import org.junit.Test;
 
+import java.util.Iterator;
+
 import static org.junit.Assert.assertEquals;
 
 public class SquirrelDoublyLinkedListTest extends SquirrelListTest {
     public SquirrelDoublyLinkedListTest() {
         super();
         mEmptySquirrelList = new SquirrelDoublyLinkedList();
+        mTestSquirrel = new Squirrel("a","b","c");
+        mTestSquirrel2 = new Squirrel ("e","f","g");
+        mTestSquirrel3 = new Squirrel ("i","j","k");
+
     }
 
     /**
@@ -16,6 +22,50 @@ public class SquirrelDoublyLinkedListTest extends SquirrelListTest {
      */
     @Test
     public void list_forwardBack() {
-        // ...
+        SquirrelDoublyLinkedList sl = (SquirrelDoublyLinkedList) mEmptySquirrelList;
+        sl.addToFront(mTestSquirrel3);
+        sl.addToFront(mTestSquirrel2);
+        sl.addToFront(mTestSquirrel);
+        BackIterator<Squirrel> i = sl.iterator();
+
+        Squirrel first = i.next();
+        Squirrel firstAgain = null;
+        while(i.hasNext()) i.next();
+        while(i.hasPrev()) firstAgain = i.prev();
+        assertEquals(first,firstAgain);
     }
+
+    @Test
+    public void list_nextAndPrev() {
+        SquirrelDoublyLinkedList sl = (SquirrelDoublyLinkedList) mEmptySquirrelList;
+        sl.addToFront(mTestSquirrel3);
+        sl.addToFront(mTestSquirrel2);
+        sl.addToFront(mTestSquirrel);
+        BackIterator<Squirrel> i = sl.iterator();
+
+        boolean check = i.next()== i.prev();
+
+        assertEquals(true, check);
+    }
+
+    @Test
+    public void list_moreWithRemove() {
+        SquirrelDoublyLinkedList sl = (SquirrelDoublyLinkedList) mEmptySquirrelList;
+        sl.addToFront(mTestSquirrel3);
+        sl.addToFront(mTestSquirrel2);
+        sl.addToFront(mTestSquirrel);
+        BackIterator<Squirrel> i = sl.iterator();
+
+        i.next();
+        i.prev();
+        i.remove();
+        i.next();
+        i.next();
+        i.prev();
+        i.remove();
+
+        assertEquals(mTestSquirrel2
+                , sl.getItem(0));
+    }
+
 }
